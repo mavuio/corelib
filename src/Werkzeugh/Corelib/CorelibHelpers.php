c48c5c7d650d61a27a0b359c0068b163fb061c57
Manfred Wuits <manfred@monochrom.at>
2015-01-28 11:37:35 +0100

    added MergeArrays

--- src/Werkzeugh/Corelib/CorelibHelpers.php
+++ src/Werkzeugh/Corelib/CorelibHelpers.php
@@ -5,6 +5,25 @@
 class CorelibHelpers
 {
 
+
+
+    public function MergeArrays($Arr1, $Arr2)
+    {
+        foreach($Arr2 as $key => $Value)
+        {
+            if(array_key_exists($key, $Arr1) && is_array($Value))
+                $Arr1[$key] = $this->MergeArrays($Arr1[$key], $Arr2[$key]);
+
+            else
+                $Arr1[$key] = $Value;
+
+        }
+
+        return $Arr1;
+
+    }
+
+    
     public function dash_case($str)
     {
         return str_replace('_','-',snake_case($str));
